87b62d2255cd00662da72505e5d664d93a005d49
jasonv268 <140399757+jasonv268@users.noreply.github.com>
2024-03-08 16:53:36 +0100

    runserver moins soumis aux erreurs

--- runserver.bat
+++ runserver.bat
@@ -9,17 +9,17 @@ if exist "%VENV_DIR%" (
     echo Création de l'environnement virtuel Python...
     python -m venv "%VENV_DIR%"
     echo Environnement virtuel Python créé avec succès.
-    echo Activation de l'environnement virtuel...
-    call "%VENV_DIR%\Scripts\activate"
-
-    echo Installation des dépendances depuis le fichier %REQUIREMENTS_FILE%...
-    pip install -r "%REQUIREMENTS_FILE%"
-
-    playwright install
-
-    echo Installation terminée.
 )
+
+echo Activation de l'environnement virtuel...
 call "%VENV_DIR%\Scripts\activate"
+
+echo Installation des dépendances depuis le fichier %REQUIREMENTS_FILE%...
+pip install -r "%REQUIREMENTS_FILE%"
+
+playwright install
+
+echo Installation terminée.
 
 if exist .config (
     echo Le fichier .config est présent.
